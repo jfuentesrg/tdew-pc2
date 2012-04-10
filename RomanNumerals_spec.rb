5a12761838a29245f9f33fbbf993e0257a123c50
javier fuentes rivera galvan <jfuentesrg@gmail.com>
2012-04-10 12:43:39 -0500

    Update RomanNumerals_spec.rb

--- RomanNumerals_spec.rb
+++ RomanNumerals_spec.rb
@@ -1,1 +1,13 @@
 require "./RomanNumerals.rb"
+
+describe RomanNumerals  do
+  
+before(:each) do
+		@roman = Roman.new
+	end
+
+	it "1 en romano es I" do
+		@roman.romano(1).should == "I"
+	end
+
+end
